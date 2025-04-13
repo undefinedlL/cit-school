f670ef4c2ab77146c123a69fd6c2faf3a6f3249f
undefinedL <anastasiasur@icloud.com>
2025-04-13 20:16:45 +0300

    commit

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -1,7 +1,7 @@
 import pages from "../pages";
 
 const routes = [
-    {index: true, element: <pages.RootPage />},
+    {path: '', element: <pages.RootPage />},
     {path: '/courses/:id', element: <pages.CoursePage/>},
     {path: '/courses', element: <pages.CoursesPage/>},
     {path: '/about', element: <pages.AboutPage/>},
